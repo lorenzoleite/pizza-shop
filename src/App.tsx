6a53b8a8a4c3f7762302b8f37f04c5c15cb2f6bf
lorenzoleite <lorenzokleite@gmail.com>
2024-02-10 02:06:08 -0300

    🎉 Begin a project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { router } from "./routes";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
+}
